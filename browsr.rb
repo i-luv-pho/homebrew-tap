8a74bf294dec217269517f8676e78a30b2adee78
i-luv-pho <pho2610ana@proton.me>
2026-01-17 11:03:53 -0800

    v2.1.0: Groq + Green Theme

--- browsr.rb
+++ browsr.rb
@@ -1,8 +1,8 @@
 class Browsr < Formula
-  desc "AI Design Studio - Create stunning HTML/CSS designs with natural language"
+  desc "FREE AI Design Generator - Hacker Green Theme"
   homepage "https://github.com/i-luv-pho/browsr"
-  url "https://github.com/i-luv-pho/browsr/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "69e4d214d929cc018cde79bf94eb626b51ad2b450042a97b259b1f7dd461ee3b"
+  url "https://github.com/i-luv-pho/browsr/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "7fa4d9e1c2303962ba8548122d59bc4dbf4b8ba6432f02e3cdc4f3ca832ffe5e"
   license "MIT"
 
   depends_on "node"
